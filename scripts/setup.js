2d3480e9e686ea266aec9f4d7ad8e6298451d655
Eduard Jacobs <edoter92@gmail.com>
2021-02-24 23:08:08 -0800

    macos setup script

---
+++ scripts/setup.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const { exec } = require("child_process");
+
+if (process.platform === "darwin") {
+  const macSdkDir = "sdk.dir = /Users/edo/Library/Android/sdk";
+  const localPropsDir = "../android/local.properties";
+
+  fs.writeFile(localPropsDir, macSdkDir, (err) => {
+    if (err) throw error;
+  });
+
+  const macEnvCmds = [
+    'export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"',
+    "export ANDROID_HOME=$HOME/Library/Android/sdk",
+    "export PATH=$PATH:$ANDROID_HOME/emulator",
+    "export PATH=$PATH:$ANDROID_HOME/tools",
+    "export PATH=$PATH:$ANDROID_HOME/tools/bin",
+    "export PATH=$PATH:$ANDROID_HOME/platform-tools",
+  ];
+
+  macEnvCmds.map((cmd) => exec(cmd));
+}
